f35c5abf1e0ed015fc19bba617fa22e1036dfe4e
ninjadev999 <mikenarkss@gmail.com>
2019-05-06 08:28:29 -0700

    updated api base url

--- config/urls.py
+++ config/urls.py
@@ -27,24 +27,22 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
 
-swagger_schema_view = get_swagger_view(title='Headshot API')
+swagger_schema_view = get_swagger_view(title='Headshot printing API')
 
 schema_view = get_schema_view(
    openapi.Info(
       title="Headshot API",
       default_version='v1',
-      description="RESTful API for www.headshot.com",
-      terms_of_service="http://www.headshot/terms/",
+      description="RESTful API for www.headshotprinting.net",
+      terms_of_service="http://www.headshotprinting.net/terms/",
       contact=openapi.Contact(email="alaric@headshot.com"),
-      license=openapi.License(name="headshot.com"),
+      license=openapi.License(name="headshotprinting.net"),
    ),
    # validators=['flex', 'ssv'],
+   url="http://www.headshotprinting.net/api/v1",
    public=True,
    permission_classes=(permissions.AllowAny,),
 )
-
-
-# schema_view = get_swagger_view(title='Headshot API')
 
 urlpatterns = [
 	# url(r'^apis', schema_view),
